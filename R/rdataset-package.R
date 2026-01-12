4b2e24a7c6718a462ae89732380c8703ad9180a1
GregJohnsonJr <gregjohn456@gmail.com>
2026-01-12 09:03:10 -0500

    adding phyloseq to the imports and adding the write_phyloseq function

---
+++ R/rdataset-package.R
@@ -0,0 +1,7 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom phyloseq otu_table
+## usethis namespace: end
+NULL
---
+++ R/write_phyloseq.R
@@ -0,0 +1,3 @@
+write_phyloseq <- function(data, dir_path = NULL, compress = TRUE, tags = NULL) {
+  
+}
