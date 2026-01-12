3a77cd85d10038629093088078a31ad18d21434c
YoungKrug <gregjohn456@gmail.com>
2026-01-12 13:19:14 -0500

    Adding test for read/write phyloseq

--- R/rdataset-package.R
+++ R/rdataset-package.R
@@ -2,6 +2,9 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom phyloseq import_mothur
 #' @importFrom phyloseq otu_table
+#' @importFrom phyloseq phy_tree
+#' @importFrom phyloseq tax_table
 ## usethis namespace: end
 NULL
---
+++ tests/testthat/test-read_phyloseq.R
@@ -0,0 +1,8 @@
+test_that("test read_phyloseq", {
+  # phyloseq_object <- import_mothur(rdataset_example("esophagus.fn.list"), rdataset_example("esophagus.good.groups"), 
+  #                         rdataset_example("esophagus.tree"))
+  
+  # data <- read_phyloseq(phyloseq_object)
+
+  
+})
