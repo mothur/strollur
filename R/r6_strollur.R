885625790465b29c4260909b9d3f83dafddca8c8
Sarah Westcott <mothur.westcott@gmail.com>
2026-03-31 15:06:15 -0400

    Adds abundance to r6_strollur

--- R/r6_strollur.R
+++ R/r6_strollur.R
@@ -169,6 +169,54 @@ strollur <- R6Class("strollur",
     },
 
     #' @description
+    #' Get the abundance data for sequences, bins, samples, and treatments.
+    #'
+    #' @param type, string containing the type of data you want the number of.
+    #'   Options include: "sequences", "bins", "samples" and "treatments".
+    #'   Default = "sequences".
+    #'
+    #' @param bin_type, string containing the bin type you would like the
+    #'   abundance data for. Default = "otu".
+    #'
+    #' @param by_sample, Boolean. When by_sample is TRUE, the abundance data
+    #'   will be parsed by sample. Default = FALSE.
+    #'
+    #' @examples
+    #'
+    #' miseq <- miseq_sop_example()
+    #'
+    #' # To the total abundance for each sequence
+    #' miseq$abundance(type = "sequences")
+    #'
+    #' # To the total abundance for each sequence parsed by sample
+    #' miseq$abundance(type = "sequences", by_sample = TRUE)
+    #'
+    #' # To the total abundance for each "otu" bin
+    #' miseq$abundance(type = "bins", bin_type = "otu")
+    #'
+    #' # To the total abundance for each "otu" bin parsed by sample
+    #' miseq$abundance(type = "bins", bin_type = "otu", by_sample = TRUE)
+    #'
+    #' # To the total abundance for each "asv" bin
+    #' miseq$abundance(type = "bins", bin_type = "asv")
+    #'
+    #' # To the total abundance for each "asv" bin parsed by sample
+    #' miseq$abundance(type = "bins", bin_type = "asv", by_sample = TRUE)
+    #'
+    #' # To the total abundance for each sample
+    #' miseq$abundance(type = "samples")
+    #'
+    #' # To the total abundance for each treatment
+    #' miseq$abundance(type = "treatments")
+    #'
+    #' @return data.frame
+    abundance <- function(type = "sequences",
+                          bin_type = "otu",
+                          by_sample = FALSE) {
+      xdev_abundance(self, type, bin_type, by_sample)
+    },
+
+    #' @description
     #' Add sequences, reports, metadata or resource references
     #'
     #' @param table, a data.frame containing the data you wish to add.
